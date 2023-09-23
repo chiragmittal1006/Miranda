e227f55e469c89394105b195815e3a29ad7f2281
chiragmittal1006 <21ucs053@lnmiit.ac.in>
2023-09-24 02:50:12 +0530

    second commit

--- script.js
+++ script.js
@@ -56,3 +56,52 @@ t.to(".page1",{
     scale:1,
     duration:0.8
 })
+var cover1 = document.querySelector(".page10 .box1")
+var cover2 = document.querySelector(".page10 .box2")
+var cover3 = document.querySelector(".page10 .box3")
+var cover4 = document.querySelector(".page10 .box4")
+
+document.querySelector(".page10 .box1").addEventListener("mousemove",()=>{
+  cover2.style.marginLeft = "37%";
+  cover3.style.marginLeft = "55.5%";
+  cover4.style.marginLeft = "74%";
+  cover1.style.backgroundColor = "#b4a899";
+
+})
+
+document.querySelector(".page10 .box1").addEventListener("mouseleave",()=>{
+  cover2.style.marginLeft = "18.5%";
+  cover3.style.marginLeft = "37%";
+  cover4.style.marginLeft = "56%";
+  cover1.style.backgroundColor = "#C4BCB2";
+})
+
+cover2.addEventListener("mousemove",()=>{
+  cover3.style.marginLeft = "55.5%";
+  cover4.style.marginLeft = "74%";
+  cover2.style.backgroundColor = "#b4a899";
+})
+
+cover2.addEventListener("mouseleave",()=>{
+  cover3.style.marginLeft = "37%";
+  cover4.style.marginLeft = "56%";
+  cover2.style.backgroundColor = "#C4BCB2";
+})
+
+cover3.addEventListener("mousemove",()=>{
+  cover4.style.marginLeft = "74%";
+  cover3.style.backgroundColor = "#b4a899";
+})
+
+cover3.addEventListener("mouseleave",()=>{
+  cover4.style.marginLeft = "56%";
+  cover3.style.backgroundColor = "#C4BCB2";
+})
+
+cover4.addEventListener("mousemove",()=>{
+  cover4.style.backgroundColor = "#b4a899";
+})
+
+cover4.addEventListener("mouseleave",()=>{
+  cover4.style.backgroundColor = "#C4BCB2";
+})
